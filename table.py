f219106117c3f70ae4d71436f5cf2f8804e7bc1c
Ian Katz <ifreecarve@gmail.com>
2013-02-11 23:55:41 -0500

    more UI widgets and animation code

--- table.py
+++ table.py
@@ -14,11 +14,6 @@ class ZenTable(Frame):
       self.xold = None
       self.yold = None
       
-      self.parent.title("Buttons")
-      
-      #frame = Frame(self, relief=RAISED, borderwidth=1)
-      #frame.pack(fill=BOTH, expand=1)
-
       drawing_area = Canvas(self.parent, width=600, height=600)
       drawing_area.pack(fill=BOTH, expand=1)
       drawing_area.bind("<Motion>",          self.h_motion)
@@ -28,14 +23,6 @@ class ZenTable(Frame):
       self.drawing_area = drawing_area
 
       self.pack(fill=BOTH, expand=1)
-      
-      self.style = Style()
-      self.style.theme_use("default")
-      closeButton = Button(self, text="Close")
-      closeButton.pack(side=RIGHT, padx=5, pady=5)
-      okButton = Button(self, text="OK")
-      okButton.pack(side=RIGHT)
-
 
    def h_b1down(self, event):
       # we only want to draw when the button is down
@@ -60,13 +47,73 @@ class ZenTable(Frame):
          self.xold = event.x
          self.yold = event.y
             
+   def animate(self):
 
+      self.animation_steps = 100
+
+      self.draw_frame()
+
+      print "Animate is done"
+
+   def draw_frame(self):
+      if 0 >= self.animation_steps:
+         print "DONE"
+      else:
+         p  = self.animation_steps
+         np = p - 1
+
+         self.drawing_area.create_line(p, p, np, np, smooth=False, fill="blue")
+         self.animation_steps = np
+
+         #self.update_idletasks()
+
+         self.after(100, self.draw_frame)
+
+class ButtonBar(Frame):
+   def __init__(self, parent, on_ok=None):
+      Frame.__init__(self, parent, relief=RAISED, borderwidth=1)
+      self.parent = parent
+      self.on_ok = on_ok
+
+      self.pack()
+      self.initUI()
+
+   def initUI(self):
+      self.style = Style()
+      self.style.theme_use("default")
+
+      close_button = Button(self, text="Close")
+      close_button.pack(side=RIGHT, padx=5, pady=5)
+      close_button.bind("<ButtonRelease-1>", self.h_close)
+
+      ok_button = Button(self, text="OK")
+      ok_button.pack(side=RIGHT)
+      ok_button.bind("<ButtonRelease-1>", self.h_ok)
+
+      self.pack(fill=BOTH, expand=False)
+
+   def h_close(self, event):
+      self.parent.destroy()
+
+   def h_ok(self, event):
+      if self.on_ok: 
+         self.on_ok()
+      print "ButtonBar.h_ok is done"
 
 def main():
-    root = Tk()
-    table = ZenTable(root)
-    root.mainloop()
+   root = Tk()
+   root.resizable(0, 0)
+   
+   frame_t = Frame(root)
+   frame_t.pack(fill=BOTH, expand=False)
+
+   table = ZenTable(frame_t)
+      
+
+   buttons = ButtonBar(root, table.animate)
+   root.mainloop()
+
 
 
 if __name__ == "__main__":
-    main()
+   main()

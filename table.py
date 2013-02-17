9ecff7c69150361783d17f25d45b47a9ac70688f
Ian Katz <ifreecarve@gmail.com>
2013-02-17 13:04:32 -0500

    initial refactor

--- table.py
+++ table.py
@@ -1,92 +1,12 @@
 from Tkinter import Tk, Canvas, RIGHT, BOTH, RAISED
 from ttk import Frame, Button, Style
 
+from zen_table import ZenTable
+
+
 TBL_WIDTH=600
 TBL_HEIGHT=600
 
-class ZenTable(Frame):
-   """
-   a UI element that allows drawing, then is drawn upon
-   """
-
-   def __init__(self, parent):
-      Frame.__init__(self, parent)
-      self.parent = parent
-      self.initUI()
-      self.pack()
-      self.rockpoint = [[False for y in range(TBL_HEIGHT)] for x in range(TBL_WIDTH)]
-
-   def initUI(self):
-      self.b1up = True
-      
-      drawing_area = Canvas(self.parent, width=TBL_WIDTH, height=TBL_HEIGHT)
-      drawing_area.pack(fill=BOTH, expand=1)
-      drawing_area.bind("<Motion>",          self.h_motion)
-      drawing_area.bind("<ButtonPress-1>",   self.h_b1down)
-      drawing_area.bind("<ButtonRelease-1>", self.h_b1up)
-
-      self.drawing_area = drawing_area
-
-      self.pack(fill=BOTH, expand=1)
-
-   def get_rockpoint(self):
-      # copy the 2D array of rock points
-      return [row[:] for row in self.rockpoint]
-
-   def h_b1down(self, event):
-      # we only want to draw when the button is down
-      # because "Motion" events happen -all the time-
-      self.b1up = False
-      self.reg_point(event)
-
-   def h_b1up(self, event):
-      self.b1up = True
-
-   def h_motion(self, event):
-      self.reg_point(event)
-
-   def reg_point(self, event):
-      if not self.b1up:
-         try:
-            self.rockpoint[event.x][event.y] = True
-         
-            event.widget.create_line(event.x,
-                                     event.y,
-                                     event.x+1,
-                                     event.y+1,
-                                     smooth=False,
-                                     fill="red")
-         except IndexError:
-            print "Ignoring %s, %s" % (event.x, event.y)
-      
-         
-   def debug(self):
-      for x, col in enumerate(self.rockpoint):
-         for y, is_point in enumerate(col):
-            if is_point:
-               print "point at %d, %d" % (x, y)
-
-   def animate(self):
-
-      self.animation_steps = 100
-
-      self.draw_frame()
-
-      print "Animate is done"
-
-   def draw_frame(self):
-      if 0 >= self.animation_steps:
-         print "DONE"
-      else:
-         p  = self.animation_steps
-         np = p - 1
-
-         self.drawing_area.create_line(p, p, np, np, smooth=False, fill="blue")
-         self.animation_steps = np
-
-         #self.update_idletasks()
-
-         self.after(100, self.draw_frame)
 
 class BoustrophedonSolver(object):
    def __init__(self, rockpoint_array):
@@ -134,7 +54,7 @@ def main():
    frame_t = Frame(root)
    frame_t.pack(fill=BOTH, expand=False)
 
-   table = ZenTable(frame_t)
+   table = ZenTable(frame_t, TBL_WIDTH, TBL_HEIGHT)
       
    def solve_boustrophedon():
       #table.debug()
---
+++ zen_table.py
@@ -0,0 +1,89 @@
+from Tkinter import Tk, Canvas, RIGHT, BOTH, RAISED
+from ttk import Frame, Button, Style
+
+
+class ZenTable(Frame):
+   """
+   a UI element that allows drawing, then is drawn upon
+   """
+
+   def __init__(self, parent, table_width, table_height):
+      Frame.__init__(self, parent)
+      self.parent = parent
+      self.table_width = table_width
+      self.table_height = table_height
+      self.initUI()
+      self.pack()
+      self.rockpoint = [[False for y in range(self.table_height)] for x in range(self.table_width)]
+
+   def initUI(self):
+      self.b1up = True
+      
+      drawing_area = Canvas(self.parent, width=self.table_width, height=self.table_height)
+      drawing_area.pack(fill=BOTH, expand=1)
+      drawing_area.bind("<Motion>",          self.h_motion)
+      drawing_area.bind("<ButtonPress-1>",   self.h_b1down)
+      drawing_area.bind("<ButtonRelease-1>", self.h_b1up)
+
+      self.drawing_area = drawing_area
+
+      self.pack(fill=BOTH, expand=1)
+
+   def get_rockpoint(self):
+      # copy the 2D array of rock points
+      return [row[:] for row in self.rockpoint]
+
+   def h_b1down(self, event):
+      # we only want to draw when the button is down
+      # because "Motion" events happen -all the time-
+      self.b1up = False
+      self.reg_point(event)
+
+   def h_b1up(self, event):
+      self.b1up = True
+
+   def h_motion(self, event):
+      self.reg_point(event)
+
+   def reg_point(self, event):
+      if not self.b1up:
+         try:
+            self.rockpoint[event.x][event.y] = True
+         
+            event.widget.create_line(event.x,
+                                     event.y,
+                                     event.x+1,
+                                     event.y+1,
+                                     smooth=False,
+                                     fill="red")
+         except IndexError:
+            print "Ignoring %s, %s" % (event.x, event.y)
+      
+         
+   def debug(self):
+      for x, col in enumerate(self.rockpoint):
+         for y, is_point in enumerate(col):
+            if is_point:
+               print "point at %d, %d" % (x, y)
+
+   def animate(self):
+
+      self.animation_steps = 100
+
+      self.draw_frame()
+
+      print "Animate is done"
+
+   def draw_frame(self):
+      if 0 >= self.animation_steps:
+         print "DONE"
+      else:
+         p  = self.animation_steps
+         np = p - 1
+
+         self.drawing_area.create_line(p, p, np, np, smooth=False, fill="blue")
+         self.animation_steps = np
+
+         #self.update_idletasks()
+
+         self.after(100, self.draw_frame)

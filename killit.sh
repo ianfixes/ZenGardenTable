9ca6e1264bf614a330e4ce567d36d0de4e9fb50b
Ian Katz <ifreecarve@gmail.com>
2013-02-17 13:07:24 -0500

    changed name in launch scripts

--- killit.sh
+++ killit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ps -ef |grep "[p]ython table.py" 
-ps -ef |grep "[p]ython table.py" |awk '{print "kill -9 " $2}' |sh
+ps -ef |grep "[p]ython table_main.py" 
+ps -ef |grep "[p]ython table_main.py" |awk '{print "kill -9 " $2}' |sh

f23769b841b41fd1e70328c6fffb3835dddf9c74
Ian <ifreecarve@gmail.com>
2014-04-06 23:57:35 -0400

    far greater efficiency through builtins, early exits, and proper caching

--- boustrophedon_solver.py
+++ boustrophedon_solver.py
@@ -1,7 +1,7 @@
 
 import math
 
-pythag = lambda x, y: math.sqrt(math.pow(x, 2) + math.pow(y, 2))
+pythag = math.hypot #lambda x, y: math.sqrt(math.pow(x, 2) + math.pow(y, 2))
 pythag2 = lambda x1, y1, x2, y2: pythag(x1 - x2, y1 - y2)
 DEBUG = False
 
@@ -14,7 +14,8 @@ class BoustrophedonSolver(object):
       self.rockpoint = rockpoint_array
       self.canvas = canvas
       self.radius = ball_radius
-
+      self.ball_coverage_template = self.get_ball_coverage_template(self.radius)
+      self.ball_shell_template = self.get_ball_shell_template(self.ball_coverage_template)
 
    def solve(self):
 
@@ -43,15 +44,46 @@ class BoustrophedonSolver(object):
             except:
                raise
 
-      print "Drawing cols", 
+      print "Drawing cols",
       for x, row in enumerate(self.rockpoint):
          print ".",
          for y, is_rock in enumerate(row):
             if covered[x][y]:
-               
+
                self.draw_point(x, y, "yellow")
 
       print
+
+
+   def get_ball_coverage_template(self, radius):
+       """
+       get a list of (x, y) points covered by a ball with self.radius
+
+       """
+
+       rr = radius - 1
+
+       out = []
+       for x in range(0 - rr, rr + 1):
+           for y in range(0 - rr, rr + 1):
+               if rr >= pythag(x, y):
+                   out.append((x,y))
+       return out
+
+
+   def get_ball_shell_template(self, template):
+       """
+       get a list of (x, y) points that touch non-ball neighbors
+
+       """
+
+       return [(x, y) for (x, y) in template
+               if not all(map(template.__contains__,
+                              [(x + 1, y),
+                               (x - 1, y),
+                               (x, y + 1),
+                               (x, y - 1)]))]
+
 
    def ball_coverage(self, ctr_x, ctr_y, radius=None):
        """
@@ -59,40 +91,59 @@ class BoustrophedonSolver(object):
 
        points that overlap the edges are considered
        """
-       
-       if None is radius:
-          rr = self.radius - 1
-       else:
-          rr = radius
 
-       out = []
-       for x in range(ctr_x - rr, ctr_x + rr + 1):
-           for y in range(ctr_y - rr, ctr_y + rr + 1):
-               if rr >= pythag(ctr_x-x, ctr_y-y):
-                   out.append((x,y))
-       return out
+       if radius is not None:
+           template = self.get_ball_coverage_template(radius)
+       else:
+           template = self.ball_coverage_template
+
+       return [(x + ctr_x, y + ctr_y) for (x, y) in template]
+
+
+
+   def ball_shell(self, ctr_x, ctr_y, template=None):
+       """
+       get a list of (x, y) points covered by the shell of a ball at given center
+       """
+       if template is not None:
+           tp = template
+       else:
+           tp = self.ball_shell_template
+
+       return [(x + ctr_x, y + ctr_y) for (x, y) in template]
+
+
 
    def is_rockpoint(self, x, y):
-      if 0 > x or 0 > y:
-         return True
-      if x >= len(self.rockpoint) or y >= len(self.rockpoint[0]):
-         return True
-      return self.rockpoint[x][y]
+       if 0 > x or 0 > y: return True
+       if x >= len(self.rockpoint): return True
+
+       col = self.rockpoint[x]
+       if y >= len(col): return True
+       return col[y]
+
 
    def displacement(self, ctr_x, ctr_y, coverage=None):
       """
       calculate the displacement of the ball from its desired center
+
+      for every rock point, draw a radius around them of un-allowable area
+      of the remaining points, the one closest to the original center is the displacement
       """
 
       # find coverage of ball
       if None is coverage:
-         coverage = self.ball_coverage(ctr_x, ctr_y, self.radius)
+         coverage = self.ball_coverage(ctr_x, ctr_y)
 
       # find any rock points within the ball
       rockpoints = []
       for (x, y) in coverage:
          if self.is_rockpoint(x, y):
             rockpoints.append((x, y))
+
+      # early exit if no rocks
+      if 0 == len(rockpoints):
+          return 0, 0
 
       if DEBUG: print "rockpoints are", rockpoints
 
@@ -119,7 +170,7 @@ class BoustrophedonSolver(object):
          x1, y1, d1 = ac1
          x2, y2, d2 = ac2
 
-         if d1 > d2: 
+         if d1 > d2:
             return ac2
          elif d1 < d2:
             return ac1
@@ -149,7 +200,7 @@ class BoustrophedonSolver(object):
 
    def draw_point(self, x, y, color="black"):
        self.canvas.create_line(x, y, x+1, y, smooth=False, fill=color)
-      
+
 
    def animate(self):
       self.animation_steps = 100
